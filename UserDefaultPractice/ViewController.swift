4ac371866821ea2e544777345fa77471772143c5
Albert <chenyihao@Alberts-MacBook-Pro.local>
2018-12-14 10:03:53 +0800

    Demo

--- UserDefaultPractice/ViewController.swift
+++ UserDefaultPractice/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let userDefault = UserDefaults()
+    
+    @IBOutlet var showMessageLabel: UILabel!
+    
+    @IBOutlet var myMessageTextField: UITextField!
+    
+    @IBAction func saveMessageButton(_ sender: UIButton) {
+        userDefault.setValue(myMessageTextField.text, forKey: "Message")
+    }
+    
+    @IBAction func readMessageButton(_ sender: UIButton) {
+        showMessageLabel.text = userDefault.value(forKey: "Message") as! String
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
